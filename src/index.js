3a9628149cf75020003d7d0253b937a72630d96a
Jimmy Wei <cmw025@bucknell.edu>
2020-03-01 20:38:42 -0500

    google analytics

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
+import ReactGA from 'react-ga';
 
+const trackingId = "UA-159254061-1"; // Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
